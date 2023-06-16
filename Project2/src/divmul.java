4db9f9366a469948a412cc4438bb06c71235f7d5
Priya Bichukale <co21priyabichukale@gmail.com>
2023-06-15 22:54:52 -1200

    2nd Class Created

---
+++ Project2/src/divmul.java
@@ -0,0 +1,13 @@
+
+public class divmul {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int div,mul,n1=4,n2=2;
+		mul=n1*n2;
+		div=n1/n2;
+		System.out.println("Mul is:"+mul);
+		System.out.println("Div is:"+div);
+	}
+
+}
