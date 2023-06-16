a0d7dd5979c4620abef82c051473f48798318dcd
Priya Bichukale <co21priyabichukale@gmail.com>
2023-06-15 22:50:03 -1200

    Updated

--- Project2/src/Test2.java
+++ Project2/src/Test2.java
@@ -2,9 +2,12 @@
 public class Test2 {
 	public static void main(String[] args) {
 		
-		int add,n1=3,n2=3;
+		int add,sub,n1=3,n2=3;
 		add=n1+n2;
 		System.out.println("Addition ia: "+add);
+		sub=n1-n2;
+		System.out.println("Substraction is: "+sub);
+		
 	}
 
 }
